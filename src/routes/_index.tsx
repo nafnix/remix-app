04a4ba4bce13116fd39b7709f843a3f95ad73837
nafnix <nafnix@proton.me>
2024-06-18 22:29:03 +0800

    feat: add unocss

--- src/routes/_index.tsx
+++ src/routes/_index.tsx
@@ -10,7 +10,7 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div>
-      <h1>Welcome to Remix</h1>
+      <h1>Welcome to Remix & UnoCSS</h1>
       <ul>
         <li>
           <a
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import { vitePlugin as remix } from "@remix-run/dev"
 import { defineConfig } from "vite"
+import UnoCSS from "unocss/vite"
 
 export default defineConfig({
-  plugins: [remix({
-    appDirectory: "src",
-    future: {
-      v3_fetcherPersist: true,
-      v3_relativeSplatPath: true,
-      v3_throwAbortReason: true,
-    },
-  })],
+  plugins: [
+    remix({
+      appDirectory: "src",
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+      },
+    }),
+    UnoCSS(),
+  ],
 })
